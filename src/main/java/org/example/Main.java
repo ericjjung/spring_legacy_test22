a7bfd901cfdf6eb2c642c8aba32c6cdfb176cfa4
eric <dudgus23@gmail.com>
2022-11-09 10:06:03 +0900

    새로운 예제 전에 푸시

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"org.example.repository"})
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication app = new SpringApplication(Main.class);
+        app.run();
+
+//        ApplicationContext context = app.run(args);
+//
+//        Environment env = context.getEnvironment();
     }
 }
