d3324db29b44521aac98661d2d12d0514416163e
eric <dudgus23@gmail.com>
2022-11-14 13:05:57 +0900

    설정들이 너무 많다 ㅠㅠ

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,3 @@
 package org.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"org.example"})
-@EntityScan(basePackages = {"org.example.domain.entity"})
-@EnableJpaRepositories(basePackages = {"org.example.repository"})
-public class Main {
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Main.class);
-        app.run();
-
-//        ApplicationContext context = app.run(args);
-//
-//        Environment env = context.getEnvironment();
-    }
-}
